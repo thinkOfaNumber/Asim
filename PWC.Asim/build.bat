9b9a6da7e95aefcacdec56e2446d5e3bec059689
Iain Buchanan <iain.buchanan@radicalsystems.com.au>
2013-06-19 23:59:04 +0000

    fixed ilmerge output type so console messages show.

--- PWC.Asim/build.bat
+++ PWC.Asim/build.bat
@@ -19,7 +19,7 @@ mkdir %BUILDDIR%\DropTests
 rem bin
 rem /targetplatform:"v4,C:\Program Files\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0"
 echo using ILMerge to build Asim.exe...
-"%ILMERGE%" /target:winexe /targetplatform:"v4,%ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:%BUILDDIR%\bin\Asim.exe PWC.Asim.ConsoleApp\bin\Release\Asim.exe PWC.Asim.Core\bin\Release\PWC.Asim.Core.dll
+"%ILMERGE%" /target:exe /targetplatform:"v4,%ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:%BUILDDIR%\bin\Asim.exe PWC.Asim.ConsoleApp\bin\Release\Asim.exe PWC.Asim.Core\bin\Release\PWC.Asim.Core.dll
 xcopy PWC.Asim.ExcelTools\bin\Release\AsimExcelTools.exe %BUILDDIR%\bin
 xcopy Algorithms\PWC.Asim.Algorithms.PvNone\bin\Release\PWC.SLMS.Algorithms.PvNone.dll %BUILDDIR%\bin
 xcopy Algorithms\PWC.Asim.Algorithms.PvSimple\bin\Release\PWC.SLMS.Algorithms.PvSimple.dll %BUILDDIR%\bin
